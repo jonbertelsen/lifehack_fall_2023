89d3756e36cd811ed7ca35eb4a23aaddd798fa83
badranyoussef <79192971+badranyoussef@users.noreply.github.com>
2023-10-11 10:51:25 +0200

    entities created

---
+++ src/main/java/app/entities/Student.java
@@ -0,0 +1,21 @@
+package app.entities;
+
+public class Student {
+
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
