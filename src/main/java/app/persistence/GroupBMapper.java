036b36ec8ca464f82070d5cfe8cf5ee3a1336519
SirRolin <nicolaieno@msn.com>
2023-10-11 12:37:16 +0200

    Update GroupBMapper.java

--- src/main/java/app/persistence/GroupBMapper.java
+++ src/main/java/app/persistence/GroupBMapper.java
@@ -5,6 +5,6 @@ import app.entities.GroupBMovie;
 import java.util.List;
 
 public interface GroupBMapper {
-    public List<GroupBMovie> getMovies(List<String> genres, List<String> ignoredGenres, float randomness, boolean ryanInMovie);
+    public List<GroupBMovie> getMovies(ConnectionPool connectionPool, List<String> genres, List<String> ignoredGenres, float randomness, boolean ryanInMovie);
 
 }
