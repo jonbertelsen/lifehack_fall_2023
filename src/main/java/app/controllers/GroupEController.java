cb2c7ee43eef50f320e730f3332280f5bc999095
DavidAH <cph-dh223@cphbusiness.dk>
2023-10-11 10:44:14 +0200

    method setup

--- src/main/java/app/controllers/GroupEController.java
+++ src/main/java/app/controllers/GroupEController.java
@@ -1,5 +1,15 @@
 package app.controllers;
+import io.javalin.http.Context;
 
 public class GroupEController
 {
+    public static void getinput(Context cxt)
+    {
+
+    }
+
+    public static int calcualtor(String startCurrentsy, String endCurrentsy, int value)
+    {
+        return -1;
+    }
 }
