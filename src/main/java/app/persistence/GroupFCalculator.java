e6f99f00d2cf9e51a0dde6987db2c53cbd37bc75
MGold <mikkelgold@protonmail.com>
2023-10-11 14:44:36 +0200

    gjort beskeden i GroupFCalculator bedre

--- src/main/java/app/persistence/GroupFCalculator.java
+++ src/main/java/app/persistence/GroupFCalculator.java
@@ -21,12 +21,12 @@ import java.util.List;
                         "and the average sugar intake is 30 grams per day, so you need to walk for "
                         + (positiveNumber * 0.9) + " minutes today to burn off the extra " + positiveNumber + " grams sugar");
             } else {
-                System.out.println("You are good since there was " + sugar + " grams of sugar in the drink, so you only had "
-                        + sugarPerDay + " grams of sugar out of 30 the grams per day");
+                System.out.println("You are good since there was " + sugar + " grams of sugar in the drink, " +
+                        "so out of the 30 grams per day, you have " + sugarPerDay + " grams of sugar left to enjoy");
             }
         }
 
-        /*public class SugarContentService {
+        /*public class SugarContent {
             public double calculateSugar(int d_id, ConnectionPool connectionPool) throws DatabaseException {
                 List<GroupFDrinks> sugarContent = GroupFMapper.getSugar(d_id, connectionPool);
                 double sugar = sugarContent.get(0).getSugar();
