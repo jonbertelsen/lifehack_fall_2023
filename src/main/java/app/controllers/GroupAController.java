de381e239734c639d5f57a1d6db1de6cbcae8906
babanoelk <124783309+babanoelk@users.noreply.github.com>
2023-10-11 12:31:46 +0200

    updated

--- src/main/java/app/controllers/GroupAController.java
+++ src/main/java/app/controllers/GroupAController.java
@@ -11,34 +11,40 @@ public class GroupAController {
     private static Team team = new Team();
     private static Random random = new Random();
 
-
+    // Funktion til at hente en tilfældig studerendes indeks
     private static int getRandomStudent() {
-        return random.nextInt(1, team.getListOfTeam().size()-1);
+        return random.nextInt(1, team.getListOfTeam().size() - 1);
     }
 
+    // Metode til at håndtere anmodningen om at finde en studerendes navn
     public static void getStudentsName(Context ctx, ConnectionPool connectionPool) {
         int result = getRandomStudent();
         String userName = ctx.formParam("username");
 
-        try{
+        try {
+            // Sæt brugernavn og partnerens navn i konteksten (context)
             ctx.attribute("username", userName);
-        ctx.attribute("partnerName",team.getListOfTeam().get(result).toString());
-        ctx.render("groupA-find-result.html");}
-        catch (Exception e){
+            ctx.attribute("partnerName", team.getListOfTeam().get(result).toString());
+
+            // Vis resultatet ved at rendere en HTML-side
+            ctx.render("groupA-find-result.html");
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void getFrontPage(Context ctx, ConnectionPool connectionPool){
+    // Metode til at håndtere anmodningen om forsiden for "Group A"
+    public static void getFrontPage(Context ctx, ConnectionPool connectionPool) {
         String teamName = team.getTeamName();
 
-        try{
+        try {
+            // Sæt holdnavnet i konteksten (context)
             ctx.attribute("teamname", teamName);
+
+            // Vis forsiden for "Group A" ved at rendere en HTML-side
             ctx.render("groupA.html");
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
     }
-
 }
--- src/main/java/app/entities/Student.java
+++ src/main/java/app/entities/Student.java
@@ -4,14 +4,17 @@ public class Student {
 
     private String name;
 
+    // Konstruktør til at initialisere en studerendes navn
     public Student(String name) {
         this.name = name;
     }
 
+    // Metode til at få den studerendes navn
     public String getName() {
         return name;
     }
 
+    // Overskrivning af toString-metoden for at returnere navnet som en streng
     @Override
     public String toString() {
         return getName();
