59604245b54e63e0c10b41306547eb49d9c43407
Jon Bertelsen <jobe@cphbusiness.dk>
2023-10-12 18:56:45 +0200

    minor

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -38,18 +38,19 @@ public class Main
         app.get("/groupA", ctx -> GroupAController.getFrontPage(ctx, connectionPool));
         app.post("/find-partner", ctx -> GroupAController.getStudentsName(ctx, connectionPool));
 
-        // Group E
-        app.post("/valuta", ctx -> GroupEController.getInput(ctx));
-        app.get("/valuta", ctx -> GroupEController.valutaInit(ctx));
-
         // Group B
 
-        app.get("/",ctx -> GroupBController.renderChoosenGenre(ctx, connectionPool));
+      //  app.get("/",ctx -> GroupBController.renderChoosenGenre(ctx, connectionPool));
         app.post("/update1", ctx -> GroupBController.updateGenreList(ctx,connectionPool));
         app.post("/update2", ctx -> GroupBController.updateIgnoreList(ctx,connectionPool));
         app.post("/update3", ctx -> GroupBController.renderChoosenGenre(ctx,connectionPool));
         app.post("/groupB.html",ctx -> GroupBController.removeSearchParameters(ctx,connectionPool));
         app.get("/actionBtn",ctx -> GroupBController.getMovieResults(ctx, connectionPool));
         app.get("/menu",ctx-> GroupBController.renderStart(ctx));
+
+        // Group E
+        app.post("/valuta", ctx -> GroupEController.getInput(ctx));
+        app.get("/valuta", ctx -> GroupEController.valutaInit(ctx));
+
     }
 }
