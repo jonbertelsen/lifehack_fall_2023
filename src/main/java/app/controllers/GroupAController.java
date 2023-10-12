7b4e763b055431fcb8268760ba1fb119cc3d3765
lassekh <31202809+lassekh@users.noreply.github.com>
2023-10-12 15:33:37 +0200

    Matching level chosen

--- src/main/java/app/controllers/GroupAController.java
+++ src/main/java/app/controllers/GroupAController.java
@@ -4,6 +4,7 @@ import app.entities.Team;
 import app.persistence.ConnectionPool;
 import io.javalin.http.Context;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class GroupAController {
@@ -12,29 +13,35 @@ public class GroupAController {
     private static Random random = new Random();
 
     // Funktion til at hente en tilfældig studerendes indeks
-    private static int getRandomStudent() {
-        return random.nextInt(1, team.getListOfTeam().size() - 1);
+    private static int getRandomStudent(String inputLevel) {
+        int result = random.nextInt(1, team.getListOfTeam().size() - 1);
+        String str = team.getListOfTeam().get(result).getLevel();
+        while (!str.equalsIgnoreCase(inputLevel)) {
+            result = random.nextInt(1, team.getListOfTeam().size() - 1);
+            str = team.getListOfTeam().get(result).getLevel();
+        }
+        return result;
     }
 
     // Metode til at håndtere anmodningen om at finde en studerendes navn
     public static void getStudentsName(Context ctx, ConnectionPool connectionPool) {
-        int result = getRandomStudent();
-
         String userName = ctx.formParam("username");
         String inputLevel = ctx.formParam("java_level");
 
-        try {
-            // Sæt brugernavn og partnerens navn i konteksten (context)
-            ctx.attribute("username", userName);
-            ctx.attribute("java_level", inputLevel);
-            ctx.attribute("partnerName", team.getListOfTeam().get(result).toString());
+        int result = getRandomStudent(inputLevel);
+
+            try {
+                // Sæt brugernavn og partnerens navn i konteksten (context)
+                ctx.attribute("username", userName);
+                ctx.attribute("java_level", inputLevel);
+                ctx.attribute("partnerName", team.getListOfTeam().get(result).toString());
 
 
-            // Vis resultatet ved at rendere en HTML-side
-            ctx.render("groupA-find-result.html");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+                // Vis resultatet ved at rendere en HTML-side
+                ctx.render("groupA-find-result.html");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
     }
 
     // Metode til at håndtere anmodningen om forsiden for "Group A"
