c1ac1de66d3e6e84b772f210d16c5b7ed3f27364
SirRolin <nicolaieno@msn.com>
2023-10-12 13:16:01 +0200

    fixing merge mistake randomness is back

--- src/main/java/app/controllers/GroupBController.java
+++ src/main/java/app/controllers/GroupBController.java
@@ -53,6 +53,7 @@ public class GroupBController
     private static void renderScearhSite(Context ctx, ConnectionPool connectionPool){
         ctx.sessionAttribute("genrelist", genreList);
         ctx.sessionAttribute("ignoredgenrelist", ignoredGenreList);
+        ctx.sessionAttribute("randomness",ctx.formParam("randomness"));
         if(genres == null){
             genres = GroupBMapperCustomizable.getStaticDistinctGenres(connectionPool);
         }
